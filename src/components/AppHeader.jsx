84d222bece4540ad47ee402848fa1df69cfd5a36
gialli24 <a.gialli2409@gmail.com>
2026-03-06 14:20:01 +0100

    Add header style

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,7 +1,24 @@
+import dcLogo from '/img/dc-logo.png'
+
 export default function AppHeader() {
     return (
         <header>
-            Header
+            <div className="container">
+                <img src={dcLogo} alt="DC Logo" className='logo' />
+
+                <nav className='text-uppercase'>
+                    <a href="#" className='nav-item'>Characters</a>
+                    <a href="#" className='nav-item active'>Comics</a>
+                    <a href="#" className='nav-item'>Movies</a>
+                    <a href="#" className='nav-item'>TV</a>
+                    <a href="#" className='nav-item'>Games</a>
+                    <a href="#" className='nav-item'>Collectibles</a>
+                    <a href="#" className='nav-item'>Videos</a>
+                    <a href="#" className='nav-item'>Fans</a>
+                    <a href="#" className='nav-item'>News</a>
+                    <a href="#" className='nav-item'>Shop</a>
+                </nav>
+            </div>
         </header>
     );
 }
