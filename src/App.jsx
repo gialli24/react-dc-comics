8e617fff153382d5c22214d95fc4615b4835c78d
gialli24 <a.gialli2409@gmail.com>
2026-03-06 12:53:11 +0100

    Create basic components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import AppFooter from './components/AppFooter'
+import AppHeader from './components/AppHeader'
+import AppMain from './components/AppMain'
 
 function App() {
 
   return (
     <>
-
+      <AppHeader />
+      <AppMain />
+      <AppFooter />
     </>
   )
 }
