5ea55ec4020bb3c34cac1f20e4791237d9f36067
gialli24 <a.gialli2409@gmail.com>
2026-03-09 10:55:01 +0100

    Update header rendering dinamically

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,22 +1,77 @@
 import dcLogo from '/img/dc-logo.png'
 
 export default function AppHeader() {
+
+    const links = [
+        {
+            id: 1,
+            text: "Characters",
+            url: "#",
+            isCurrent: false
+        },
+        {
+            id: 2,
+            text: "Comics",
+            url: "#",
+            isCurrent: true
+        },
+        {
+            id: 3,
+            text: "Movies",
+            url: "#",
+            isCurrent: false
+        },
+        {
+            id: 4,
+            text: "TV",
+            url: "#",
+            isCurrent: false
+        },
+        {
+            id: 5,
+            text: "Games",
+            url: "#",
+            isCurrent: false
+        },
+        {
+            id: 6,
+            text: "Collectibles",
+            url: "#",
+            isCurrent: false
+        },
+        {
+            id: 7,
+            text: "Fans",
+            url: "#",
+            isCurrent: false
+        },
+        {
+            id: 8,
+            text: "News",
+            url: "#",
+            isCurrent: false
+        },
+        {
+            id: 9,
+            text: "Shop",
+            url: "#",
+            isCurrent: false
+        }
+    ]
+
     return (
         <header>
             <div className="container">
                 <img src={dcLogo} alt="DC Logo" className='logo' />
 
                 <nav className='text-uppercase'>
-                    <a href="#" className='nav-item'>Characters</a>
-                    <a href="#" className='nav-item active'>Comics</a>
-                    <a href="#" className='nav-item'>Movies</a>
-                    <a href="#" className='nav-item'>TV</a>
-                    <a href="#" className='nav-item'>Games</a>
-                    <a href="#" className='nav-item'>Collectibles</a>
-                    <a href="#" className='nav-item'>Videos</a>
-                    <a href="#" className='nav-item'>Fans</a>
-                    <a href="#" className='nav-item'>News</a>
-                    <a href="#" className='nav-item'>Shop</a>
+
+                    {
+                        links.map((link) => (
+                            <a href={link.url} className={link.isCurrent ? "nav-item active" : "nav-item"}>{link.text}</a>
+                        ))
+                    }
+
                 </nav>
             </div>
         </header>
