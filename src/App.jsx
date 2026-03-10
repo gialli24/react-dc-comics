47cf9d97c0e1856ba2e033f0c2309f86e0ad5b0c
gialli24 <a.gialli2409@gmail.com>
2026-03-10 12:03:44 +0100

    Centralize data in src/assets/js/data.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import AppFooter from './components/AppFooter'
 import AppHeader from './components/AppHeader'
 import AppMain from './components/AppMain'
+import { comics } from './assets/js/data.js'
+import { headerLinks } from './assets/js/data.js'
+import { footerLinks } from './assets/js/data.js'
 
 function App() {
 
   return (
     <>
-      <AppHeader />
-      <AppMain />
-      <AppFooter />
+      <AppHeader links={headerLinks} />
+      <AppMain comics={comics} />
+      <AppFooter links={footerLinks} />
     </>
   )
 }
--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,63 +1,6 @@
 import dcLogo from '/img/dc-logo.png'
 
-export default function AppHeader() {
-
-    const links = [
-        {
-            id: 1,
-            text: "Characters",
-            url: "#",
-            isCurrent: false
-        },
-        {
-            id: 2,
-            text: "Comics",
-            url: "#",
-            isCurrent: true
-        },
-        {
-            id: 3,
-            text: "Movies",
-            url: "#",
-            isCurrent: false
-        },
-        {
-            id: 4,
-            text: "TV",
-            url: "#",
-            isCurrent: false
-        },
-        {
-            id: 5,
-            text: "Games",
-            url: "#",
-            isCurrent: false
-        },
-        {
-            id: 6,
-            text: "Collectibles",
-            url: "#",
-            isCurrent: false
-        },
-        {
-            id: 7,
-            text: "Fans",
-            url: "#",
-            isCurrent: false
-        },
-        {
-            id: 8,
-            text: "News",
-            url: "#",
-            isCurrent: false
-        },
-        {
-            id: 9,
-            text: "Shop",
-            url: "#",
-            isCurrent: false
-        }
-    ]
+export default function AppHeader({ links }) {
 
     return (
         <header>
